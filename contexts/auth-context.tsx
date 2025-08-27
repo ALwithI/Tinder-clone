b03538c4eebaf81b2e316cae575af81aa61cf815
ALwithI <155818448+ALwithI@users.noreply.github.com>
2025-08-27 23:01:52 +0100

    adding the context

--- contexts/auth-context.tsx
+++ contexts/auth-context.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import { createClient } from "@/lib/supabase/client";
 import { User } from "@supabase/supabase-js";
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 
 interface AuthContextType {
   user: User | null;
@@ -11,6 +12,22 @@ interface AuthContextType {
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export function AuthProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+    const supabase = createClient();
+  const [loading, setLoading] = useState<boolean>(false);
+useEffect(() =>{
+    async function checkUser{
+        try{
+            const{data:{session}}=await supabase.auth.getSession();
+            setUser(session?.user ?? null)
+        }catch(error){
+            console.error
+        }finally{
+            setLoading(false)
+        }
+    }
+checkUser();
+})
   return (
     <AuthContext.Provider value={{ user, loading, signOut }}>
       {children}
