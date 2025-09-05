4cbe8700750cb722c931874adb21f61330b6751f
ALwithI <155818448+ALwithI@users.noreply.github.com>
2025-09-05 17:39:12 +0100

    starting the main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,15 @@ import { useAuth } from "@/contexts/auth-context";
 import Link from "next/link";
 
 export default function Home() {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-pink-500"></div>
+      </div>
+    );
+  }
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
